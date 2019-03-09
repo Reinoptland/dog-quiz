938521b34ff9c33512c056eb20eedde885e7d334
reinoptland <reinoptland@gmail.com>
2019-03-09 21:55:09 +0100

    Add breeds reducer, add test for SET_BREEDLIST

---
+++ src/reducers/breeds.js
@@ -0,0 +1,11 @@
+import { SET_BREEDLIST } from "../actions/api";
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case SET_BREEDLIST: 
+            return action.payload
+    
+        default:
+            return state
+    }
+}
