9c828a99aea1373ef086e8c394649de3ecc8cd2d
reinoptland <reinoptland@gmail.com>
2019-03-10 14:30:35 +0100

    Add tests for SHOW_FEEDBACK & HIDE_FEEDBACK

---
+++ src/actions/feedback.js
@@ -0,0 +1,10 @@
+export const SHOW_FEEDBACK = 'SHOW_FEEDBACK'
+export const HIDE_FEEDBACK = 'HIDE_FEEDBACK'
+
+export const showFeedback = () => ({
+    type: SHOW_FEEDBACK
+})
+
+export const hideFeedback = () => ({
+    type: HIDE_FEEDBACK
+})
--- src/reducers/feedback.js
+++ src/reducers/feedback.js
@@ -1,10 +1,18 @@
+import { HIDE_FEEDBACK, SHOW_FEEDBACK } from "../actions/feedback";
+
 const initialState = {
     displayFeedback: false
 }
 
 export default (state = initialState, action = {}) => {
-    switch (action.payload) {
-        
+    switch (action.type) {
+        case HIDE_FEEDBACK: {
+            return { ...state, displayFeedback: false }
+        }
+
+        case SHOW_FEEDBACK: {
+            return { ...state, displayFeedback: true }
+        }
     
         default: { 
             return state
